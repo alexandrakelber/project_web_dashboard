137b538f3d31c4014d680cdd120afd3865339237
LinnJuge <linnjugesundell@gmail.com>
2025-03-11 19:09:03 +0100

    Update scraper.sh

--- scraper.sh
+++ scraper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Variables
 API_KEY="e3de4a3649f43d096433bbcd70d28644"
@@ -27,7 +27,10 @@ weather_desc=$(echo $response | jq -r '.weather[0].description')
 
 # Vérifier si le fichier weather_data.txt existe, sinon le créer
 if [ ! -f weather_data.txt ]; then
+  echo "Le fichier weather_data.txt n'existe pas. Création du fichier."
   touch weather_data.txt
+else
+  echo "Le fichier weather_data.txt existe déjà."
 fi
 
 # Stocker les informations dans un fichier texte
@@ -40,3 +43,18 @@ echo "Humidity: $humidity%"
 echo "Pressure: $pressure hPa"
 echo "Wind Speed: $wind_speed m/s"
 echo "Weather Description: $weather_desc"
+echo "Le fichier weather_data.txt a été mis à jour."
+
+# Ajouter la date et l'heure au fichier de données
+echo "Job exécuté à : $(date)" >> weather_data.txt
+
+# Calculer les statistiques du jour (exemples)
+max_temp=$(awk -F',' '{if($1 > max_temp) max_temp=$1} END {print max_temp}' weather_data.txt)
+min_temp=$(awk -F',' '{if($1 < min_temp) min_temp=$1} END {print min_temp}' weather_data.txt)
+avg_humidity=$(awk -F',' '{sum+=$3} END {print sum/NR}' weather_data.txt)
+
+# Stocker ces informations dans un fichier de rapport
+echo "Max Temp: $max_temp" >> daily_report.txt
+echo "Min Temp: $min_temp" >> daily_report.txt
+echo "Average Humidity: $avg_humidity%" >> daily_report.txt
+
