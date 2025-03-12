12c47dea15d0bc0c1eabeea6072695cef047b290
LinnJuge <linnjugesundell@gmail.com>
2025-03-12 08:53:07 +0100

    Update dashboard.py
    Amélioration du dashboard, ajout de vilels, dropdowns et graphiques qui fonctionnent (j'espère)

--- dashboard.py
+++ dashboard.py
@@ -5,23 +5,40 @@ import plotly.express as px
 from dash.dependencies import Input, Output
 
 # Charger les données scrappées
-def load_data():
-    # Lire le fichier weather_data.txt pour afficher les dernières données
-    data = pd.read_csv('weather_data.txt', header=None, names=["temp", "feels_like", "humidity", "pressure", "wind_speed", "weather_desc"])
-    return data
+def load_data(city="London"):
+    data = pd.read_csv('weather_data.txt', header=None, names=["city", "temp", "feels_like", "humidity", "pressure", "wind_speed", "weather_desc", "temp_max", "temp_min"])
+    return data[data["city"] == city]
+
+# Charger le rapport quotidien
+def load_daily_report():
+    with open('daily_report.txt', 'r') as file:
+        return file.readlines()
 
 # Créer l'application Dash
 app = dash.Dash(__name__)
 
 # Créer un graphique pour afficher la série temporelle des températures
-def create_figure():
-    data = load_data()
-    fig = px.line(data, x=data.index, y='temp', title='Temperature over Time')
+def create_figure(city="London"):
+    data = load_data(city)
+    fig = px.line(data, x=data.index, y='temp', title=f'Temperature over Time in {city}')
     return fig
 
 # Définir la disposition du dashboard
 app.layout = html.Div([
     html.H1("Weather Dashboard"),
+
+    # Dropdown pour sélectionner la ville
+    dcc.Dropdown(
+        id='city-dropdown',
+        options=[
+            {'label': 'London', 'value': 'London'},
+            {'label': 'New York', 'value': 'New York'},
+            {'label': 'Paris', 'value': 'Paris'},
+        ],
+        value='London',
+    ),
+
+    # Affichage des données en temps réel
     html.Div([
         html.P(id="temp-display"),
         html.P(id="feels-like-display"),
@@ -30,12 +47,22 @@ app.layout = html.Div([
         html.P(id="wind-speed-display"),
         html.P(id="weather-display"),
     ]),
+
+    # Graphiques
     dcc.Graph(id='temp-graph'),
+
+    # Affichage du rapport quotidien
+    html.Div([
+        html.H3("Daily Report"),
+        html.Div(id='daily-report')
+    ]),
+
+    # Interval pour rafraîchir les données
     dcc.Interval(
         id='interval-component',
-        interval=5*60*1000,  # Interval de 5 minutes
+        interval=5*60*1000,  # Rafraîchissement toutes les 5 minutes
         n_intervals=0
-    )  # Ajout de l'interval pour rafraîchir toutes les 5 minutes
+    )
 ])
 
 # Callback pour mettre à jour les éléments du dashboard
@@ -46,12 +73,15 @@ app.layout = html.Div([
      Output('pressure-display', 'children'),
      Output('wind-speed-display', 'children'),
      Output('weather-display', 'children'),
-     Output('temp-graph', 'figure')],
-    [Input('interval-component', 'n_intervals')]
+     Output('temp-graph', 'figure'),
+     Output('daily-report', 'children')],  # Ajouter le rapport quotidien à l'affichage
+    [Input('interval-component', 'n_intervals'),
+     Input('city-dropdown', 'value')]  # Nouvelle entrée pour la ville
 )
-def update_dashboard(n_intervals):
+
+def update_dashboard(n_intervals, city):
     # Charger les données chaque fois que la fonction est appelée
-    data = load_data()
+    data = load_data(city)
 
     # Créer les éléments du dashboard
     temp = f"Temperature: {data['temp'].iloc[-1]} °C"
@@ -62,12 +92,14 @@ def update_dashboard(n_intervals):
     weather_desc = f"Weather: {data['weather_desc'].iloc[-1]}"
 
     # Créer un graphique pour la température
-    fig = create_figure()
+    fig = create_figure(city)
 
-    return temp, feels_like, humidity, pressure, wind_speed, weather_desc, fig
+    # Charger le rapport quotidien
+    report = load_daily_report()
+
+    return temp, feels_like, humidity, pressure, wind_speed, weather_desc, fig, html.Div([html.P(line) for line in report])
 
 # Exécuter le serveur
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
+    print("Dash server is running...")
+    app.run_server(debug=True, host='0.0.0.0', port=8052)
